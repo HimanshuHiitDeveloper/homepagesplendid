77019846791b8ddd8c03eaddf1e2840da8564ff8
My Name <himanshu.mehra@hiitambala.com>
2023-07-26 15:42:13 +0530

    Initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,202 @@
+'use client';
+
+import React,{useState} from 'react'
+import person4 from "./images/person4.jpg";
+import column from "./images/column.svg";
+import row from "./images/row.svg";
+import dots from "./images/dots.svg";
 import Image from 'next/image'
 
-export default function Home() {
+
+
+function Card() {
+  const [isDragging, setIsDragging] = useState(false);
+  const [loading, setLoading] = useState(false)
+  const toggle=() => {
+     if (typeof window === "object") {
+   var doc1 = document.querySelector(".profile_data")
+    var btn = document.querySelector(".profile_dropdown_btn")
+doc1.classList.toggle("active")
+  }
+  } 
+
+  function handleDragStart(e) {
+    setIsDragging(true);
+    const data = JSON.stringify({ type: "card" });
+    e.dataTransfer.setData("text/plain", data);
+  }
+
+  function handleDragEnd(e) {
+    setIsDragging(false);
+    e.dataTransfer.clearData();
+  }
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  
+    
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    <div className="m-6 wd h-45 p-2 br"  draggable
+      onDragStart={handleDragStart}
+      onDragEnd={handleDragEnd}>
+                   <div className="flex justify-between relative items-center -top-12">
+                   <Image className="p-px w-16 h-16 rounded-full" src={person4} />
+               
+                   <div className="profile_dropdown">
+                      <div className="profile_dropdown_btn" onClick={toggle}>
+                       <div className="profile_img">
+                          <Image className="w-9 h-9" src={dots}/>
+                      </div>
+                       <span >
+                       
+                       </span>
+                      </div>
+                             <ul className="profile_data">
+                                 <li>home</li>
+                                 <li>home</li>
+                                 <li>home</li>
+                             </ul>  
+                      </div>
+                   </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+                   <div className="usercontent">
+              
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+                   <h4 className="text-gray-700 font-extrabold">userName</h4>
+                <h4 className="text-gray-700 font-extrabold">Hellouser@gmail.com</h4>
+                 <p>Hellouser@gmail.com</p>
+                   </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+                 </div> 
+                 
+   
+  );
+}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+function Box({ card, moveCard,id,text }) {
+  const [isOver, setIsOver] = useState(false);
+
+  function handleDragOver(e) {
+    if (e.dataTransfer.types[0] === "text/plain") {
+      setIsOver(true);
+      e.preventDefault();
+    }
+  }
+
+  function handleDrop(e) {
+    const dataJSON = e.dataTransfer.getData("text/plain");
+    let data;
+    try {
+      data = JSON.parse(dataJSON);
+    } catch {}
+    if (data && data.type === "card") {
+      moveCard();
+    }
+  }
+
+  function handleDragLeave() {
+    setIsOver(false);
+  }
+
+  return (
+    <div
+      className="wid_300px h-4/5 Border-1 border-zinc-500 br_left"
+      onDragOver={handleDragOver}
+      onDrop={handleDrop}
+      onDragLeave={handleDragLeave}
+      id={id}
+    >
+    <div className='font-semibold text-gray-700 relative px-2 py-2 '> 
+    <h3 className="flex font-semibold items-center justify-between aft">{text} <span className="head_span">92</span></h3>
+</div>
+      {card ? <Card /> : " "}
+    </div>
+  );
+}
+
+
+export default function Home() {
+   const [index, setIndex] = useState(1);
+
+  function moveCard(i) {
+    setIndex(i);
+  }
+   const scrollings=(e)=>{
+     if (typeof window === "object") {
+    let wrapping = document.getElementById("wrapping")
+  
+       let form   =  Math.floor(e.clientX) / 2  ;
+       let px = form;
+       var mouseX = e.pageX += 200;
+      wrapping.scrollLeft = `${mouseX}`;
+      }
+ }
+
+ const onLeave=()=>{
+   if (typeof window === "object") {
+      let squre = document.getElementsByClassName('squre')[0]
+  squre.style.display = "none"
+}
+ }
+ 
+  return (
+      <>
+
+
+    <div className="w-full h-screen flex justify-center flex-col items-center bg_col p-2 ">
+       <div className="w-full flex justify-around flex-row flex-wrap items-center ">
+                <h2 className="head_col text-4xl flex justify-between items-center" >Students <span className="span_col m-2 px-2 py-2">92</span></h2>
+                
+                <input type="text" className="w-100px p-2 pl-1.7 br" >
+                 </input>
+              <div className="stu_side_section"> 
+                 <button id="filter">
+                 <span>
+             
+               </span>
+
+                 Filters</button>
+                 
+                 <div className="p-1 flex border-solid border-1 rounded-3xl br"> 
+                    <span className="row_box">
+                    <a  >
+                      <Image src={column} />
+                      </a>
+                    </span>
+                     <span className="colm_box">
+                          
+                            <Image src={row} />
+                    </span>
+                  </div>
+                 <button id="add"> 
+                <span> </span>
+                 Add New Student </button>
+                 </div>
+             </div>
+
+
+             <div className="d_grid overflow-x-auto relative" id="wrapping">
+       
+                <Box card={index === 1} text="New Lead" id="1"  moveCard={moveCard.bind(null, 1)} ></Box>
+      <Box card={index === 2} id="2" text="Follw up" moveCard={moveCard.bind(null, 2)} ></Box>
+      <Box card={index === 3} id="3"  text="Ready to Apply"  moveCard={moveCard.bind(null, 3)}></Box>
+       <Box card={index === 4} id="4" text="Updated" moveCard={moveCard.bind(null, 4)}></Box>
+       <Box card={index === 5} id="5" text="Pending" moveCard={moveCard.bind(null, 5)}></Box>
+
+ 
+             </div>
+             <div className="colm_counter" onMouseMove={scrollings} onMouseLeave={onLeave} id="scrolling">
+                      <div className="sm_col squre"> </div>
+                      <div className="sm_col"> </div>
+                      <div className="sm_col"> </div>
+                      <div className="sm_col"> </div>
+                       <div className="sm_col"> </div>
+                      <div className="sm_col"> </div>
+                      <div className="sm_col"> </div>
+                      <div className="sm_col"> </div>
+                 </div>
+   </div> 
+      </>
+    
   )
 }
